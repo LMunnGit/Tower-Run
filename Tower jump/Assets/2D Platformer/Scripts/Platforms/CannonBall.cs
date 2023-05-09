cc2f58b1b42edccc6bf66d6fc2e0ad72abce729f
LMunnGit <lazylevi77@gmail.com>
2023-05-09 14:55:13 -0700

    cannon ball raycast
    raycast is annoying

--- Tower jump/Assets/2D Platformer/Scripts/Platforms/CannonBall.cs
+++ Tower jump/Assets/2D Platformer/Scripts/Platforms/CannonBall.cs
@@ -6,23 +6,35 @@ public class CannonBall : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private bool isUp;
-    private float rot;
+    private float dir;
     // Update is called once per frame
     void FixedUpdate()
     {
         if (isUp)
         {
-            rot = 1f;
+            dir = 1f;
         } else
         {
-            rot = -1f;
+            dir = -1f;
         }
         float step = speed * Time.deltaTime;
-        Vector3 target = Vector3.up * rot * 10;
+        Vector3 target = Vector3.up * dir;
         transform.position = Vector3.MoveTowards(transform.position, target, step);
 
-        // Add raycasting
-    }
+        float raySize = 0.2f;
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, target); // check for collider
+        Debug.DrawRay(transform.position, target * raySize, Color.white);
 
+        if (hitInfo.collider != null)
+    {
+    if (hitInfo.distance < 0.05f && hitInfo.collider.gameObject.tag != "Enemy") // check for collision
+    {
+        Destroy(gameObject);
+    }
+    }
+}
+
+    
 
 }
+
